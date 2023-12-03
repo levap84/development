0e015db7502ad8d9d30f2323cf868181e9fae5cf
levap <levap84@gmail.com>
2023-12-03 14:59:49 +0100

    ukol kralovstvi splnen

---
+++ kralovstvi/mapa.php
@@ -0,0 +1,28 @@
+<?php
+$mapa = array(
+    array(
+        '<img src="pictures/hrad.png">',
+        '<img src="pictures/les2.png">',
+        '<img src="pictures/les.png">',
+        '<img src="pictures/les2.png">',
+        '<img src="pictures/rybnik.png">'
+    ),
+    
+    array(
+        '',
+        '',
+        '<img src="pictures/les2.png">',
+        '',
+        ''
+    ),
+    
+    array(
+        '',
+        '<img src="pictures/kopec.png">',
+        '<img src="pictures/les.png">',
+        '<img src="pictures/dum.png">',
+        ''
+    )
+
+);
+?>
