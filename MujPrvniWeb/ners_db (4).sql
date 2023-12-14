91ce1aad360f584fc3611950c229d77b24e0c052
levap <levap84@gmail.com>
2023-12-14 17:29:31 +0100

    cgb

---
+++ MujPrvniWeb/ners_db (4).sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Počítač: 127.0.0.1
+-- Vytvořeno: Čtv 14. pro 2023, 17:29
+-- Verze serveru: 10.4.32-MariaDB
+-- Verze PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databáze: `ners_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `clanky`
+--
+
+CREATE TABLE `clanky` (
+  `clanky_id` int(11) NOT NULL,
+  `titulek` varchar(255) NOT NULL,
+  `obsah` text NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `popisek` varchar(255) NOT NULL,
+  `klicova_slova` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `uzivatele`
+--
+
+CREATE TABLE `uzivatele` (
+  `uzivatele_id` int(11) NOT NULL,
+  `jmeno` varchar(255) NOT NULL,
+  `heslo` varchar(255) NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+--
+-- Vypisuji data pro tabulku `uzivatele`
+--
+
+INSERT INTO `uzivatele` (`uzivatele_id`, `jmeno`, `heslo`, `admin`) VALUES
+(1, 'Petr', '$2y$10$6fs1Y.RbtrE94W52G.mKp.6B81CCkZipxgqv2SbsXIM.0f9.cQVia', 1),
+(2, 'Lojza', '$2y$10$GqNDS69Ui4agD0wy5EkS7ewx7Ja7Fjs3vRY2kF7W6rqAIqnbqbDlm', 0);
+
+--
+-- Indexy pro exportované tabulky
+--
+
+--
+-- Indexy pro tabulku `clanky`
+--
+ALTER TABLE `clanky`
+  ADD PRIMARY KEY (`clanky_id`);
+
+--
+-- Indexy pro tabulku `uzivatele`
+--
+ALTER TABLE `uzivatele`
+  ADD PRIMARY KEY (`uzivatele_id`);
+
+--
+-- AUTO_INCREMENT pro tabulky
+--
+
+--
+-- AUTO_INCREMENT pro tabulku `clanky`
+--
+ALTER TABLE `clanky`
+  MODIFY `clanky_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pro tabulku `uzivatele`
+--
+ALTER TABLE `uzivatele`
+  MODIFY `uzivatele_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
